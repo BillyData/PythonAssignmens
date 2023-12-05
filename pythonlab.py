f6aef77946af55d8f42ed2d85c8d896c0e6dec51
BillyData <khoi20082005@gmail.com>
2023-12-05 15:48:18 +0700

    Add solution to lab assignment

---
+++ pythonlab.py
@@ -0,0 +1,24 @@
+# exercises
+
+# Exercise 2.1 - Exercise 1
+x = 15
+y = 30
+z = x + y
+print(z)
+
+# Exercise 2.2 - Exercise 2
+x = 5.7
+y = 43
+
+z = x + y
+print(type(z))
+
+# Exercise 2.3 - Exercise 3
+a = 1
+b = 2
+c = 3
+d = 4
+
+result = a + b + c + d
+print(result)
+print(type(result))
